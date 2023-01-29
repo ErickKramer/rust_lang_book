c10637bd67b084d6a0f3f24a499fbcfa289a42bb
Erick Kramer <erickkramer@gmail.com>
2023-01-29 21:48:16 +0100

    Complete fibonacci exercise
    Practiced writing unit tests and control flows.

--- ch03/fibonacci/src/main.rs
+++ ch03/fibonacci/src/main.rs
@@ -1,3 +1,54 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Enter number of fibonacci numbers to print");
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to catch input given");
+
+        let choice: u32 = match choice.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Input must be a number");
+                continue;
+            }
+        };
+
+        println!("============================================================");
+        for i in 0..choice {
+            let value = fibonacci(i);
+            println!("Fibonacci of {i} is {value}");
+        }
+        println!("============================================================");
+    }
+}
+
+/// Computes the fibonacci number corresponding to a given value
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        let value: u32 = fibonacci(n - 1) + fibonacci(n - 2);
+        return value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::fibonacci;
+
+    #[test]
+    fn test_fibonacci() {
+        assert_eq!(fibonacci(0), 0);
+        assert_eq!(fibonacci(1), 1);
+        assert_eq!(fibonacci(2), 1);
+        assert_eq!(fibonacci(3), 2);
+        assert_eq!(fibonacci(12), 144);
+        assert_eq!(fibonacci(17), 1597);
+        assert_eq!(fibonacci(19), 4181);
+    }
 }
--- ch03/temperatures/src/main.rs
+++ ch03/temperatures/src/main.rs
@@ -17,14 +17,14 @@ mod tests {
 
     #[test]
     fn test_fahrenheit() {
-        assert_eq!(fahrenheit_celsius(32.0), 0.0);
+        assert_float_eq!(fahrenheit_celsius(32.0), 0.0, r2nd <= 0.000_1);
         assert_float_eq!(fahrenheit_celsius(15.0), -9.444, r2nd <= 0.000_1);
         assert_float_eq!(fahrenheit_celsius(50.0), 10.0, r2nd <= 0.000_1);
     }
 
     #[test]
     fn test_celsius() {
-        assert_eq!(celsius_fahrenheit(0.0), 32.0);
+        assert_float_eq!(celsius_fahrenheit(0.0), 32.0, r2nd <= 0.000_1);
         assert_float_eq!(celsius_fahrenheit(20.0), 68.0, r2nd <= 0.000_1);
         assert_float_eq!(celsius_fahrenheit(10.0), 50.0, r2nd <= 0.000_1);
     }
