02dc0df65b1229ceef5789feae31656398346923
Erick Kramer <erickkramer@gmail.com>
2023-03-08 22:19:10 +0100

    Improve task1 implementation
    changes:
    - Handle cases where there are more than number equal to `max_freq`
    - Rename most_repeated_value to compute_mode
    - Add test case for the compute_median with an unsorted input

--- ch08/exercises/task1/src/main.rs
+++ ch08/exercises/task1/src/main.rs
@@ -18,7 +18,7 @@ fn compute_median(input: &[i32]) -> Result<f32, &str> {
     }
 }
 
-fn most_repeated_value(input: &[i32]) -> Result<i32, &str> {
+fn compute_mode(input: &[i32]) -> Result<Vec<i32>, &str> {
     if input.is_empty() {
         return Err("Cannot calculate the most repeated value of an empty vector");
     }
@@ -30,17 +30,14 @@ fn most_repeated_value(input: &[i32]) -> Result<i32, &str> {
         *count += 1;
     }
 
-    let mut max_count = 0;
-    let mut most_repeated_value = 0;
-    // iterate over the keys and counts as references as we only need to read their values
-    for (&key, &count) in &values {
-        if count > max_count {
-            most_repeated_value = key;
-            max_count = count;
+    let max_count = *values.values().max().unwrap();
+    let mut mode: Vec<i32> = vec![];
+    for (key, count) in values {
+        if count == max_count {
+            mode.push(key);
         }
-
     }
-    return Ok(most_repeated_value);
+    return Ok(mode);
 }
 
 fn main() {
@@ -52,13 +49,14 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::most_repeated_value;
+    use crate::compute_mode;
 
     use super::compute_median;
 
     #[test]
     fn test_compute_median() {
         assert_eq!(compute_median(&vec![1, 3, 3, 6, 7, 8, 9]), Ok(6.0));
+        assert_eq!(compute_median(&vec![9, 8, 7, 1, 3, 3, 6]), Ok(6.0));
         assert_eq!(compute_median(&vec![1, 2, 3, 4, 5, 6, 8, 9]), Ok(4.5));
         assert_eq!(
             compute_median(&vec![]),
@@ -67,12 +65,13 @@ mod tests {
     }
 
     #[test]
-    fn test_most_repeated_value() {
+    fn test_compute_mode() {
         assert_eq!(
-            most_repeated_value(&vec![]),
+            compute_mode(&vec![]),
             Err("Cannot calculate the most repeated value of an empty vector")
         );
-        assert_eq!( most_repeated_value(&vec![1,2,3,3,4,4,4]), Ok(4));
-        assert_eq!( most_repeated_value(&vec![1]), Ok(1));
+        assert_eq!(compute_mode(&vec![1, 2, 3, 3, 4, 4, 4]), Ok(vec![4]));
+        assert_eq!(compute_mode(&vec![1, 2, 3, 3, 4, 4]), Ok(vec![3, 4]));
+        assert_eq!(compute_mode(&vec![1]), Ok(vec![1]));
     }
 }
