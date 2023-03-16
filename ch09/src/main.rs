dcb5cb68559f9cbde40169a1eb9167fb00ad045d
Erick Kramer <erickkramer@gmail.com>
2023-03-16 08:43:57 +0100

    Add examples from chapter 9

---
+++ ch09/src/main.rs
@@ -0,0 +1,59 @@
+use std::{
+    fs::{File, self},
+    io::{self, ErrorKind, Read},
+};
+
+/// Propagate error -- verbose approach
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+/// Propagate error using the ? operator
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+/// Propagate error using the ? chained
+fn read_username_from_file_shortest() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+/// read file using the std library
+fn read_username_from_file_native() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+}
