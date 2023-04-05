af48b2ce17166a2e65bddc111d7278494c1d3bbc
Erick Kramer <erickkramer@gmail.com>
2023-04-05 22:17:39 +0200

    Follow along the minigrep project

---
+++ ch12/minigrep/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    // std::env::args will panic if any arguments contains invalid Unicode
+    // The output of args is an iterator that gets converted into a collection via the collect
+    // method
+    let args: Vec<String> = env::args().collect();
+    // If an error occurs it runs the code in the anonymous function (aka closure)
+    let config = Config::build(&args).unwrap_or_else(|_err| {
+        process::exit(1);
+    });
+
+    // we use `if let` as we are not interested in the success output of the run method, but in the
+    // presence of an error
+    if let Err(e) = minigrep::run(config) {
+        process::exit(1);
+    }
+}
